4c1657609593e2a6e6ea1ba2f400ab1844c81799
Gaurang Bharti <gaurangbharti@gmail.com>
2021-02-21 02:58:59 +0530

    First Commit
    Initial Commit - Uploading all major files

---
+++ lower_upper_detection(not working).py
@@ -0,0 +1,45 @@
+import numpy as np
+import cv2
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+upper_body = cv2.CascadeClassifier('haarcascade_upperbody.xml')
+lower_body = cv2.CascadeClassifier('haarcascade_lowerbody.xml')
+
+
+cap = cv2.VideoCapture(0)
+
+while 1:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    upper = upper_body.detectMultiScale(gray, 1.3, 5)
+    lower = lower_body.detectMultiScale(gray, 1.3, 5)
+    
+
+    # for (x,y,w,h) in faces:
+    #     cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    #     roi_gray = gray[y:y+h, x:x+w]
+    #     roi_color = img[y:y+h, x:x+w]
+        
+    #     eyes = eye_cascade.detectMultiScale(roi_gray)
+    #     for (ex,ey,ew,eh) in eyes:
+    #         cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            
+    for (x,y,w,h) in upper:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+    
+    for (x,y,w,h) in lower:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+
+    cv2.imshow('img',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
